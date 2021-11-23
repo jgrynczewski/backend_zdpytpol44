de5b0bd8deac2575b296b5596ceda612ba328ed5
jgrynczewski <Tel^)%322))$>
2021-11-23 21:08:44 +0100

    Dane od użytkownika cz1

--- hello/urls.py
+++ hello/urls.py
@@ -3,6 +3,10 @@ from django.urls import path
 from hello import views
 
 urlpatterns = [
-    path('home/', views.hello),
-    path('home2/', views.hello2),
+    path('home/', views.hello_view),
+    path('home2/', views.hello2_view),
+    path('adam/', views.adam_view),
+    path('ewa/', views.ewa_view),
+    # path('<str:name>/', views.name_view),  # Konwerter scieżki
+    path('<str:name>/', views.name_view2),
 ]
--- hello/views.py
+++ hello/views.py
@@ -2,9 +2,33 @@ from django.shortcuts import HttpResponse
 from django.shortcuts import render
 
 
-def hello(request):
+def hello_view(request):
     return HttpResponse("<!DOCTYPE html><html><head><title>Hello</title></head><body><h1>Hello, world!</h1></body></html>")
 
 
-def hello2(request):
+def hello2_view(request):
     return render(request, 'hello.html')
+
+
+def adam_view(request):
+    return HttpResponse("Witaj, Adam!")
+
+
+def ewa_view(request):
+    return HttpResponse("Witaj, Ewa!")
+
+
+def name_view(request, name):
+    from markupsafe import escape
+    print(escape(name))
+    return HttpResponse(f"Witaj, {escape(name)}!")
+
+
+def name_view2(request, name):
+    return render(
+        request,
+        'name.html',
+        context={
+            'name': name
+        }
+    )
