822dc851c2b8e4e9e69ff6f25aa62530679b624b
jgrynczewski <Tel^)%322))$>
2021-11-24 19:05:11 +0100

    warunki cz1

--- isitchristmas/views.py
+++ isitchristmas/views.py
@@ -1,8 +1,21 @@
+import datetime
 from django.shortcuts import render
 
 # Create your views here.
 def index(request):
+    now = datetime.datetime.now()
+    month = now.month
+    day = now.day
+
+    if month == 12 and day == 25:
+        is_it_christmas = True
+    else:
+        is_it_christmas = False
+
     return render(
         request,
-        'isitchristmas/hello.html',
+        'isitchristmas/index.html',
+        context={
+            'is_it_christmas': is_it_christmas,
+        }
     )
