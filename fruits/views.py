1a7030bfce4778db420365c2683eaf3f907762fa
jgrynczewski <Tel^)%322))$>
2021-11-29 18:57:07 +0100

    jezyk szablonow django - cd

--- fruits/views.py
+++ fruits/views.py
@@ -1,5 +1,9 @@
 from django.shortcuts import render
 
+class Dog:
+    def __init__(self, name):
+        self.name = name
+
 # Create your views here.
 def index(request):
     fruits = [
@@ -9,10 +13,18 @@ def index(request):
         'jablko',
     ]
 
+
     return render(
         request,
         'fruits/index.html',
         context={
             'fruits': fruits,
+            'fruit': 'banan',
+            'text': "<p style='color:red;'>Hello</p>",
+            'passenger': {
+                'name': "Bob",
+                'age': 30,
+            },
+            'dog': Dog('Azor'),
         }
     )
