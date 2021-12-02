d50bfc1c6f6b10e911cf76da207bb4a3e53f1470
jgrynczewski <Tel^)%322))$>
2021-12-02 19:36:06 +0100

    crud part 2

--- form_app6/urls.py
+++ form_app6/urls.py
@@ -7,4 +7,5 @@ app_name = 'form_app6'
 urlpatterns = [
     path('register/', views.register, name='register'),
     path('tasks-list/', views.tasks_list, name='tasks-list'),
+    path('update/<int:task_id>/', views.update, name='update'),
 ]
--- form_app6/views.py
+++ form_app6/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 from django.shortcuts import redirect
+from django.core.exceptions import ObjectDoesNotExist
+from django.shortcuts import Http404
+from django.shortcuts import get_object_or_404
 
 from form_app6.models import Task
 
@@ -26,5 +29,24 @@ def tasks_list(request):
         'form_app6/tasks-list.html',
         context={
             'tasks': tasks,
+        }
+    )
+
+
+def update(request, task_id):
+
+    # try:
+    #     task = Task.objects.get(id=task_id)
+    # except ObjectDoesNotExist:
+    #     raise Http404()
+
+    task = get_object_or_404(Task, id=task_id)
+
+
+    return render(
+        request,
+        'form_app6/update.html',
+        context={
+            'task': task,
         }
     )
