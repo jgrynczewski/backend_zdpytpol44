bd8c21b555ddec13714dde8ca0aaa394e100760a
jgrynczewski <Tel^)%322))$>
2021-12-02 18:53:18 +0100

    crud part 1

--- form_app6/urls.py
+++ form_app6/urls.py
@@ -1,3 +1,10 @@
-urlpatterns = [
+from django.urls import path
 
+from form_app6 import views
+
+app_name = 'form_app6'
+
+urlpatterns = [
+    path('register/', views.register, name='register'),
+    path('tasks-list/', views.tasks_list, name='tasks-list'),
 ]
--- form_app6/views.py
+++ form_app6/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.shortcuts import redirect
 
-# Create your views here.
+from form_app6.models import Task
+
+
+def register(request):
+    task = request.POST.get('task')
+    if task:
+        Task.objects.create(text=task)
+
+        return redirect('form_app6:tasks-list')
+
+    return render(
+        request,
+        'form_app6/register.html',
+    )
+
+
+def tasks_list(request):
+
+    tasks = Task.objects.all()
+
+    return render(
+        request,
+        'form_app6/tasks-list.html',
+        context={
+            'tasks': tasks,
+        }
+    )
