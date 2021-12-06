597e3d9729ef1e22fab940c6f2eacdc7315b2069
jgrynczewski <Tel^)%322))$>
2021-12-06 20:36:42 +0100

    OneToMany

--- relations/admin.py
+++ relations/admin.py
@@ -2,7 +2,11 @@ from django.contrib import admin
 
 from relations.models import Country
 from relations.models import Capital
+from relations.models import Language
+from relations.models import Framework
 
 # Register your models here.
 admin.site.register(Country)
 admin.site.register(Capital)
+admin.site.register(Language)
+admin.site.register(Framework)
--- relations/models.py
+++ relations/models.py
@@ -19,3 +19,18 @@ class Capital(models.Model):
 # class Country(models.Model):
 #     name = models.CharField(max_length=128)
 #     capital = models.OneToOneField(Capital, on_delete=models.CASCADE)
+
+
+class Language(models.Model):
+    name = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.name
+
+
+class Framework(models.Model):
+    name = models.CharField(max_length=128)
+    language = models.ForeignKey(Language, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
