be21b2b281eb56da868461fe94a61542f82085d7
jgrynczewski <Tel^)%322))$>
2021-12-06 21:01:17 +0100

    ManyToMany

--- relations/admin.py
+++ relations/admin.py
@@ -4,9 +4,13 @@ from relations.models import Country
 from relations.models import Capital
 from relations.models import Language
 from relations.models import Framework
+from relations.models import Movie
+from relations.models import Character
 
 # Register your models here.
 admin.site.register(Country)
 admin.site.register(Capital)
 admin.site.register(Language)
 admin.site.register(Framework)
+admin.site.register(Movie)
+admin.site.register(Character)
--- relations/models.py
+++ relations/models.py
@@ -34,3 +34,18 @@ class Framework(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Movie(models.Model):
+    name = models.CharField(max_length=128)
+
+    def __str__(self):
+        return self.name
+
+
+class Character(models.Model):
+    name = models.CharField(max_length=128)
+    movies = models.ManyToManyField(Movie)
+
+    def __str__(self):
+        return self.name
