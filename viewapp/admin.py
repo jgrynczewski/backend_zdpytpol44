04ba0a6a5b4e07fa8830096731158302aaba723a
jgrynczewski <Tel^)%322))$>
2021-12-09 18:35:08 +0100

    Panel administratora - modyfikacje

--- viewapp/admin.py
+++ viewapp/admin.py
@@ -3,4 +3,9 @@ from django.contrib import admin
 from viewapp.models import Person
 
 # Register your models here.
-admin.site.register(Person)
+# admin.site.register(Person)
+
+# Reprezentacja modelu w panelu admina
+@admin.register(Person)
+class PersonAdmin(admin.ModelAdmin):
+    list_display = ("name", "age", "city")
